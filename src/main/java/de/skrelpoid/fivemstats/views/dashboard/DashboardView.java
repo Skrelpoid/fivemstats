67bda722fcddc1c322b9a1234d6d489f8760b28c
Dustin <skrelpoid@protonmail.com>
2023-07-02 16:17:35 +0200

    dashboard

--- src/main/java/de/skrelpoid/fivemstats/views/dashboard/DashboardView.java
+++ src/main/java/de/skrelpoid/fivemstats/views/dashboard/DashboardView.java
@@ -2,6 +2,8 @@ package de.skrelpoid.fivemstats.views.dashboard;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,13 +21,11 @@ import com.github.appreciated.apexcharts.config.plotoptions.builder.BarBuilder;
 import com.github.appreciated.apexcharts.config.theme.Mode;
 import com.github.appreciated.apexcharts.helper.Series;
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.Scroller.ScrollDirection;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -47,64 +47,98 @@ public class DashboardView extends HorizontalLayout {
 	private static final Logger logger = LoggerFactory.getLogger(DashboardView.class);
 
 	private static final long serialVersionUID = 1L;
-	private final TextField name;
-	private final Button sayHello;
-	private final ApexCharts charts;
+	private ApexCharts charts;
 
-	private final Scroller scroller;
+	private Scroller scroller;
+	private final Div progress;
+
+	private final PlayerLogService playerLogService;
+	private final PlayerService playerService;
 
 	public DashboardView(final PlayerLogService playerLogService, final PlayerService playerService) {
-		name = new TextField("Your name");
-		sayHello = new Button("Say hello");
-		sayHello.addClickListener(e -> Notification.show("Hello " + name.getValue()));
-		sayHello.addClickShortcut(Key.ENTER);
-
+		this.playerLogService = playerLogService;
+		this.playerService = playerService;
 		setMargin(true);
-		setVerticalComponentAlignment(Alignment.END, name, sayHello);
-		final Map<Long, Player> players = playerService.findAllAndGroupByID();
-		// TODO limit
-		final List<PlayerLogSeconds> data = playerLogService.calculateAllLoggedInTime().subList(0, 50);
 
-		// TODO show players missing with 0
-
-		final ApexChartsBuilder builder = new ApexChartsBuilder();
-		builder.withTooltip(TooltipBuilder.get().withTheme("dark").build());
-		builder.withTheme(ThemeBuilder.get().withMode(Mode.DARK).withPalette("palette1").build());
-
-		final Chart chart = new Chart();
-		chart.setType(Type.BAR);
-		chart.setHeight(String.valueOf(data.size() * 16));
-		chart.setBackground("transparent");
-
-		final XAxis xaxis = new XAxis();
-		xaxis.setCategories(
-				data.stream().map(PlayerLogSeconds::getPlayerId).map(id -> players.get(id).getName()).toList());
-
-		builder.withChart(chart);
-		builder.withSeries(new Series<>("Minutes Logged", data.stream().map(PlayerLogSeconds::getCumulatedSeconds)
-				.map(Long::doubleValue).map(d -> Math.max(Math.round(d / 60), 1D)).toArray(Double[]::new)));
-		builder.withXaxis(xaxis);
-		builder.withDataLabels(DataLabelsBuilder.get().withEnabled(true).build());
-		builder.withPlotOptions(
-				PlotOptionsBuilder.get().withBar(BarBuilder.get().withHorizontal(true).build()).build());
-
-		scroller = new Scroller();
-		scroller.setId("chartScroller");
-		charts = builder.build();
-		charts.setWidthFull();
-		scroller.setContent(charts);
-		scroller.setScrollDirection(ScrollDirection.VERTICAL);
-		scroller.setWidthFull();
-		scroller.setHeight("50%");
 		setHeightFull();
-		add(name, sayHello, scroller);
+		progress = buildProgress();
+		
+		add(progress);
+	}
+	
+	private Div buildProgress() {
+		final Div container = new Div();
+		container.addClassName("progressContainer");
+		container.setSizeFull();
+		
+		final Label label = new Label("Loading Data, please stand by...");
+		container.add(label);
+		
+		final Div spinner = new Div();
+		spinner.addClassName("spinner");
+		
+		final Div cube1 = new Div();
+		cube1.addClassName("cube1");
+		
+		final Div cube2 = new Div();
+		cube2.addClassName("cube2");
+		
+		spinner.add(cube1, cube2);
+		container.add(label, spinner);
+		return container;
 	}
 
 	@Override
 	protected void onAttach(final AttachEvent attachEvent) {
 		super.onAttach(attachEvent);
-		final Element element = scroller.getElement();
-		element.executeJs("setTimeout(function() {$1.scrollLeft += 1000;}, 1000)", element);
+		final Executor exec = Executors.newSingleThreadExecutor();
+
+		exec.execute(() -> {
+
+			final Map<Long, Player> players = playerService.findAllAndGroupByID();
+			// TODO limit
+			final List<PlayerLogSeconds> data = playerLogService.calculateAllLoggedInTime();
+
+			// TODO show players missing with 0
+
+			final ApexChartsBuilder builder = new ApexChartsBuilder();
+			builder.withTooltip(TooltipBuilder.get().withTheme("dark").build());
+			builder.withTheme(ThemeBuilder.get().withMode(Mode.DARK).withPalette("palette1").build());
+
+			final Chart chart = new Chart();
+			chart.setType(Type.BAR);
+			chart.setHeight(String.valueOf(data.size() * 16));
+			chart.setBackground("transparent");
+
+			final XAxis xaxis = new XAxis();
+			xaxis.setCategories(
+					data.stream().map(PlayerLogSeconds::getPlayerId).map(id -> players.get(id).getName()).toList());
+
+			builder.withChart(chart);
+			builder.withSeries(new Series<>("Minutes Logged", data.stream().map(PlayerLogSeconds::getCumulatedSeconds)
+					.map(Long::doubleValue).map(d -> Math.max(Math.round(d / 60), 1D)).toArray(Double[]::new)));
+			builder.withXaxis(xaxis);
+			builder.withDataLabels(DataLabelsBuilder.get().withEnabled(true).build());
+			builder.withPlotOptions(
+					PlotOptionsBuilder.get().withBar(BarBuilder.get().withHorizontal(true).build()).build());
+
+			scroller = new Scroller();
+			scroller.setId("chartScroller");
+
+			attachEvent.getUI().access(() -> {
+				charts = builder.build();
+				charts.setWidthFull();
+				scroller.setContent(charts);
+				scroller.setScrollDirection(ScrollDirection.VERTICAL);
+				scroller.setWidthFull();
+				scroller.setHeight("50%");
+				remove(progress);
+				add(scroller);
+				final Element element = scroller.getElement();
+				element.executeJs("setTimeout(function() {$1.scrollLeft += 1000;}, 1000)", element);
+				attachEvent.getUI().push();
+			});
+		});
 	}
 
 }
