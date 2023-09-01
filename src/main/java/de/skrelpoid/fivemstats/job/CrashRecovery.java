39ab03cc01d97c85eb749b2d72be55fa1e338dac
skrel <skrel@host.docker.internal>
2023-09-02 00:23:23 +0200

    Fix crashrecovery date when server starts up in under 1 minute

--- src/main/java/de/skrelpoid/fivemstats/job/CrashRecovery.java
+++ src/main/java/de/skrelpoid/fivemstats/job/CrashRecovery.java
@@ -2,11 +2,9 @@ package de.skrelpoid.fivemstats.job;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import de.skrelpoid.fivemstats.data.entity.Heartbeat;
 import de.skrelpoid.fivemstats.data.entity.PlayerLog;
 import de.skrelpoid.fivemstats.data.service.HeartbeatRepository;
@@ -15,11 +13,11 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 /**
- * If the application crashed (or shut down when people were still logged in and
- * they couldn't be logged out), this class will make sure on startup to apply a
- * sensible log out time, which will either be the log in time + 1 min or the
- * last active time of the service logged in the heartbeat table
- * 
+ * If the application crashed (or shut down when people were still logged in and they couldn't be
+ * logged out), this class will make sure on startup to apply a sensible log out time, which will
+ * either be the log in time + 1 min or the last active time of the service logged in the heartbeat
+ * table
+ *
  * @author dustinkristen
  *
  */
@@ -39,16 +37,19 @@ public class CrashRecovery {
 	@PostConstruct
 	void init() {
 		logger.info("start crashrecovery");
-		final Heartbeat lastKnownHeartbeat = heartbeatRepository.findAll().stream().findFirst().orElse(new Heartbeat(LocalDateTime.MIN));
+		var now = LocalDateTime.now();
+		final Heartbeat lastKnownHeartbeat =
+				heartbeatRepository.findAll().stream().findFirst().orElse(new Heartbeat(LocalDateTime.MIN));
 		final LocalDateTime heartbeatTime = lastKnownHeartbeat.getLastActiveTime();
-		
+
 		final List<PlayerLog> activeLogs = playerLogService.getActiveLogs();
 		for (final PlayerLog playerLog : activeLogs) {
 			final var logInTime = playerLog.getLogInTime();
 			if (logInTime.isBefore(heartbeatTime)) {
 				playerLog.setLogOutTime(heartbeatTime);
 			} else {
-				final var logOutTime = logInTime.plusMinutes(1);
+				final var logInPlus1 = logInTime.plusMinutes(1);
+				final var logOutTime = logInPlus1.isAfter(now) ? now : logInPlus1;
 				playerLog.setLogOutTime(logOutTime);
 			}
 		}
@@ -56,9 +57,9 @@ public class CrashRecovery {
 		finished = true;
 		logger.info("finished crashrecovery");
 	}
-	
+
 	@PreDestroy
-    public void onShutdown() {
+	public void onShutdown() {
 		logger.info("start log out on shutdown");
 		final LocalDateTime now = LocalDateTime.now();
 		final List<PlayerLog> activeLogs = playerLogService.getActiveLogs();
