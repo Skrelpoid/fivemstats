f09ff4cf62b047ea8d91ca20666157f286f18457
skrel <skrel@host.docker.internal>
2023-09-01 23:16:39 +0200

    configure target server

--- src/main/java/de/skrelpoid/fivemstats/FiveMStatsApplication.java
+++ src/main/java/de/skrelpoid/fivemstats/FiveMStatsApplication.java
@@ -1,15 +1,14 @@
 package de.skrelpoid.fivemstats;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
@@ -17,8 +16,8 @@ import jakarta.ws.rs.client.WebTarget;
 /**
  * The entry point of the Spring Boot application.
  *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
+ * Use the @PWA annotation make the application installable on phones, tablets and some desktop
+ * browsers.
  *
  */
 @SpringBootApplication
@@ -29,17 +28,18 @@ public class FiveMStatsApplication implements AppShellConfigurator {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(final String[] args) {
-        SpringApplication.run(FiveMStatsApplication.class, args);
-    }
+		SpringApplication.run(FiveMStatsApplication.class, args);
+	}
 
-    @Bean
-    WebTarget webTarget() {
-    	final Client client = ClientBuilder.newClient(); // NOSONAR close on shutdown
-    	return client.target("http://cl-rp.de:30120/players.json");
-    }
-    
-    @Bean
-    ObjectMapper objectMapper() {
-    	return new ObjectMapper();
-    }
+	@Bean
+	WebTarget webTarget(@Value("${fivemstats.target.hostname}") final String server,
+			@Value("${fivemstats.target.port:30120}") final Integer port) {
+		final Client client = ClientBuilder.newClient(); // NOSONAR close on shutdown
+		return client.target("http://" + server + ":" + port + "/players.json");
+	}
+
+	@Bean
+	ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
